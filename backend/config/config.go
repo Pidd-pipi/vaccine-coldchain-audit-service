b28b0cfeea0bdbf9f88844b970422f317905fab6
Pidd-pipi <Pidd-pipi@users.noreply.github.com>
2026-08-26 04:29:51 +0800

    feat: initial import

---
+++ backend/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Port      int
+	ExportDir string
+}
+
+func Load() Config {
+	p := 8080
+	if v, e := strconv.Atoi(os.Getenv("PORT")); e == nil && v > 0 && v < 65536 {
+		p = v
+	}
+	exportDir := os.Getenv("EXPORT_DIR")
+	if exportDir == "" {
+		exportDir = "./output"
+	}
+	return Config{Port: p, ExportDir: exportDir}
+}
+func (c Config) Address() string { return fmt.Sprintf(":%d", c.Port) }
